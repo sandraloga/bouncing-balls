94d14b0b3c456d303a5c7654023eef00dbf176c7
Sandra <72sandraloga@gmail.com>
2016-03-31 20:03:49 +0200

    bolas con colores y diametros aleatorios

--- BallDemo.java
+++ BallDemo.java
@@ -1,6 +1,5 @@
 import java.awt.Color;
 import java.util.Random;
-import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -13,7 +12,7 @@ import java.util.ArrayList;
 public class BallDemo   
 {
     private Canvas myCanvas;
-    private ArrayList<BouncingBall> bolas;
+    private BouncingBall[] bolas;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -21,17 +20,33 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
-        bolas = new ArrayList<>();
-       
+
+    }
+    /**
+     * Cuando se invoque debe dibujar un rectángulo sin relleno en la pantalla y tantas bolas como se indiquen como parámetro
+     * en la invocación del método dentro de dicho rectángulo.
+     *Los objetos bola de esta actividad no serán del tipo BouncingBall: lo que debes hacer es crear una nueva clase denominada BoxBall 
+     *(basándote en la clase BouncingBall, evidentemente). Las bolas de tipo BoxBall llevan siempre la misma velocidad (1 pixel cada vez),
+     *dirección oblicua (no pueden moverse ni en horizontal ni en vertical) y no les afecta la gravedad. La posición inicial, el color y la
+     *direccion de cada bola debe fijarse de manera aleatoria.
+     *Finalmente, consigue que las bolas de tipo "BoxBall" reboten cambiando de dirección cuando golpeen las paredes del rectángulo
+     */
+    public void boxBounce(int numeroBolas)
+    {
+        myCanvas.setVisible(true);
+        myCanvas.fillRectangle(20, 20, 500, 350);
+        myCanvas.eraseRectangle(20, 20, 490, 340);
+
     }
 
     /**
      * Simulate two bouncing balls
      */
+
     public void bounce(int numeroBolas)
     {
         int ground = 400;   // position of the ground line
-
+        bolas = new BouncingBall[numeroBolas];
         myCanvas.setVisible(true);
         Random aleatorio = new Random(30);
 
@@ -40,27 +55,31 @@ public class BallDemo
 
         // crate and show the balls
         for (int index = 0 ; index <numeroBolas ; index++){
-            BouncingBall ball = new BouncingBall(50, 50, aleatorio.nextInt(), Color.BLUE, ground, myCanvas);
+            
+            int radio = aleatorio.nextInt(20) + 5;
+            int red = aleatorio.nextInt(256);
+            int blue = aleatorio.nextInt(256);
+            int green = aleatorio.nextInt(256);
+            
+            Color color = new Color(red, blue, green);
+            BouncingBall ball = new BouncingBall(50+(30*index), 50,radio, color, ground, myCanvas);
             ball.draw();
-            bolas.add(ball);
+            bolas[index] = ball;
         }
         //BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
         //ball2.draw();
 
         // make them bounce
         boolean finished =  false;
-       
-            
-            while(!finished) {
-                 for( int i=0 ; i < bolas.size() ; i++) {
-                
-                bolas.get(i).move();
-                
-                myCanvas.wait(50);           // small delay
-                //ball.move();
-                //ball2.move();
+
+        while(!finished) {
+            myCanvas.wait(50); 
+            for( int i=0 ; i < numeroBolas ; i++) {
+
+                bolas[i].move();
+
                 // stop once ball has travelled a certain distance on x axis
-                if(bolas.get(i).getXPosition() >= 550) {
+                if(bolas[i].getXPosition() >= 550) {
                     finished = true;
                 }
             }
